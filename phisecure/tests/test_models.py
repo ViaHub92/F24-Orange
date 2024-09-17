eaa33871ca3596e66f3ae7595efed9689934a16d
rmpan001 <rmpan001@odu.edu>
2024-09-17 00:38:23 -0400

    Comments in test_models
    Added comments to test_model file

--- phisecure/tests/test_models.py
+++ phisecure/tests/test_models.py
@@ -2,33 +2,57 @@ import pytest
 from database.models import Role, User, Inbox, Email
 from database.db_connection import create_app, db
 
-# Define SQLite configuration for testing
+
 class TestConfig:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+    """
+    Configuration for Flask testing.
+
+    Sets up an in-memory SQLite database
+
+    Attributes:
+    SQLALCHEMY_DATABASE_URI (str): URI for the SQLite database.
+    SQLALCHEMY_TRACK_MODIFICATIONS (bool): Disables modification tracking.
+    TESTING (bool): Enables testing mode.
+    """
+
+    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     TESTING = True
 
-@pytest.fixture(scope='module')
+
+@pytest.fixture(scope="module")
 def app():
-    app = create_app(TestConfig)
+    """
+    Provides a Flask application instance for testing.
     
+    """
+    app = create_app(TestConfig)
+
     with app.app_context():
         db.create_all()
         yield app
         db.session.remove()
         db.drop_all()
-        
-@pytest.fixture(scope='module')
+
+
+@pytest.fixture(scope="module")
 def client(app):
+    """
+    Provides a test client for the Flask Application.
+    """
     return app.test_client()
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope="function")
 def db_session(app):
+    """
+    Provides a database session for testing.
+    """
     with app.app_context():
         db.session.begin_nested()
         yield db.session
         db.session.rollback()
+
 
 class TestModels:
     """
@@ -44,15 +68,13 @@ class TestModels:
         role = Role(name="admin")
         db_session.add(role)
         db_session.commit()
-        
+
         retrieved_role = Role.query.filter_by(name="admin").first()
-        
 
         # Assert that the string representation of role object matches
         assert retrieved_role is not None
         assert retrieved_role.name == "admin"
         assert role.name == "admin"
-
 
     def test_role_in_db(self, db_session):
         """
@@ -61,8 +83,7 @@ class TestModels:
         role = Role(name="student")
         db_session.add(role)
         db_session.commit()
-        
-        
+
         retrieved_role = Role.query.get(role.id)
         assert retrieved_role is not None
         assert retrieved_role.name == "student"
