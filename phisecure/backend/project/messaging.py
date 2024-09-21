691322a1295bd76a4f67e39d7fc89cdca24f3188
Mr-Koldin109 <hpollock64@gmail.com>
2024-09-21 18:10:51 -0400

    Basic functions for new db backend logic added.

--- phisecure/backend/project/messaging.py
+++ phisecure/backend/project/messaging.py
@@ -6,8 +6,25 @@ Purpose: Contains logic for messaging between accounts and their email environme
 """
 
 from project import db
-from project.models import User, Email  # Import models from Flask app
+from project.models import User, Email, Inbox  # Import models from Flask app
 from datetime import datetime, timezone
+
+# Function to check the account's email
+def check_email(acc): 
+    inbox = Inbox.query.get(acc.inbox_id)
+    print(inbox)
+    return
+
+# Function to check the account for unread emails
+def check_for_unread_emails(acc):
+    inbox = Inbox.query.get(acc.inbox_id)
+    results = inbox.query.get(is_read=False)
+    print(results)
+    return
+
+# Function to send an email to a given account
+def send_email(email, acc):
+    return
 
 # Function to manage the current user's account
 def manage_account(acc):
@@ -41,11 +58,12 @@ def manage_account(acc):
         print("4. Exit")
         user_input = input("Please give a numeric input from 1-4 to perform an action: ")
         if user_input == "1":
-            acc.check_email()  # You can modify this to fetch emails from the database
+            check_email(acc)  # You can modify this to fetch emails from the database
         elif user_input == "2":
-            acc.check_for_unread_emails()  # Modify this to check unread emails in the database
+            check_for_unread_emails(acc)  # Modify this to check unread emails in the database
         elif user_input == "3":
-            acc.make_email(test_account_list)  # This could trigger email creation and DB commit
+            send_email(test_email, test_student2)
+            # This could trigger email creation and DB commit
         elif user_input == "4":
             print("Goodbye!\n")
             break
