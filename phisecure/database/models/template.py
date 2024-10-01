9056bc41b2d880f82b3233e451a86ae660b4233b
rmpan001 <rmpan001@odu.edu>
2024-10-01 09:29:54 -0400

    Added more models
    created analytics model and phishing email to differentiate between actual legitimate emails and phishing emails.

--- phisecure/database/models/template.py
+++ phisecure/database/models/template.py
@@ -15,11 +15,14 @@ class Template(db.Model):
     Columns:
     id: id for the template.
     name: the name of the template.
+    description: description of the template.
+    subject: subject line of the template.
     body: content of the template.
     """
     __tablename__ = "templates"
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), nullable=False)
+    name = db.Column(db.String(120), nullable=False, unique=True, index=True)
     description = db.Column(db.String(120), nullable=False)
+    subject = db.Column(db.String(), nullable=False)
     body = db.Column(db.String(120), nullable=False)
     
