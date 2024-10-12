8d6bfd25f1906c6091dc2771fc94570eac52328f
Dylan Via <dvia001@odu.edu>
2024-10-11 21:24:40 -0400

    fixed

--- phisecure/tests/test_api.py
+++ phisecure/tests/test_api.py
@@ -1,25 +1,23 @@
 import pytest
 from backend.project import create_app, db
-from database.models.user import User
-from database.models.inbox import Inbox
+from backend.config import TestConfig
 
-@pytest.fixture
+@pytest.fixture(scope='session')
 def app():
-    app = create_app()
-    app.config['TESTING'] = True
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'  # In-memory database for tests
-
+    app = create_app(TestConfig)
     with app.app_context():
-        db.create_all()  # Create all tables in the test database
+        db.create_all()
         yield app
         db.session.remove()
-        db.drop_all()  # Drop all tables after tests
 
-@pytest.fixture
+@pytest.fixture(scope='function')
 def client(app):
-    return app.test_client()
+    with app.test_client() as client:
+        yield client
 
 def test_account(client):
+    from database.models.user import User
+    
     user_data = {
         "username": "testuser",
         "email": "testuser@example.com",
