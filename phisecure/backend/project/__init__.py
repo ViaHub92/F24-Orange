70f544f356038ba6bd2d3a7140b2be37db6f4f4e
Dylan Via <dvia001@odu.edu>
2024-10-11 22:49:38 -0400

    created new (non-functional) blueprint performance.py

--- phisecure/backend/project/__init__.py
+++ phisecure/backend/project/__init__.py
@@ -20,9 +20,11 @@ def create_app(config_class=Config):
     from backend.project.routes import routes
     from backend.project.blueprints.account import account
     from backend.project.blueprints.messaging import messaging
+    from backend.project.blueprints.performance import performance
     app.register_blueprint(routes)
     app.register_blueprint(account, url_prefix='/account')
     app.register_blueprint(messaging, url_prefix='/messaging')
+    app.register_blueprint(performance, url_prefix='/performance')
 
     """
     with app.app_context():
