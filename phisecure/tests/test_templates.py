dbe2b2993d3eebc9aea5c1381576f46643e940ed
rmpan001 <rmpan001@odu.edu>
2024-10-25 21:37:08 -0400

    fixed bug in template table
    fixed bug in template table that was throwing a 400 status error in create template endpoint.

--- phisecure/tests/test_templates.py
+++ phisecure/tests/test_templates.py
@@ -18,4 +18,26 @@ def client(app):
         
 
 def test_phishing_template(client):
-    pass
+    """ Test creating a phishing template endpoint
+    Args:
+        client (_type_): Flask test client
+    """
+    template_data = {
+        "name": "Test Phishing Template",
+        "description": "Test phishing template for testing purposes",
+        "category": "Test",
+        "tags": "Test, Phishing",
+        "difficulty_level": "beginner",
+        "sender": "testuser123@gmail.com",
+        "recipient": "phiseduser123@gmail.com",
+        "subject": "Test phishing email",
+        "body": "This is a test phishing email",
+        "link": "https://fakephishinglink.com"
+    }
+    
+    response = client.post('/phishing/templates', json=template_data)
+    
+    assert response.status_code == 201, "Template created successfully"
+    assert response.get_json()["template"]["name"] == "Test Phishing Template"
+    assert response.get_json()["template"]["description"] == "Test phishing template for testing purposes"
+    
