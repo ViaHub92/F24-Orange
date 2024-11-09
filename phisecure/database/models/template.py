fbece21462c6cfddaaed44bec3e58d3506641548
rmpan001 <rmpan001@odu.edu>
2024-11-09 15:03:03 -0500

    added columns to template table
    added a sender and red flag column to template table. Migrated changes to db.

--- phisecure/database/models/template.py
+++ phisecure/database/models/template.py
@@ -41,9 +41,11 @@ class Template(db.Model):
     category = db.Column(db.String(250))
     tags = db.Column(db.String(120), nullable=False, index=True)
     difficulty_level = db.Column(Enum(DifficultyLevel), nullable=False)
+    sender_template = db.Column(db.String(120), nullable=False)
     subject_template = db.Column(db.String(120), nullable=False)
     body_template = db.Column(db.String(500), nullable=False)
     link = db.Column(db.String(100))
+    template_redflag=db.Column(db.Text)
     
     phishing_emails = db.relationship("PhishingEmail", back_populates="template", lazy=True)
     
