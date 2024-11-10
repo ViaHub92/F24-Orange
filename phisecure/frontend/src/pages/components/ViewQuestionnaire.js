3ce523520a0d84cf07e010e4c32f8ab63d2e61e5
rmpan001 <rmpan001@odu.edu>
2024-11-09 20:34:32 -0500

    Update ViewQuestionnaire.js
    added icons to the questions pertaining to social media services

--- phisecure/frontend/src/pages/components/ViewQuestionnaire.js
+++ phisecure/frontend/src/pages/components/ViewQuestionnaire.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { FaSnapchat, FaInstagram, FaTwitter } from "react-icons/fa";
 import '../../styles/styles.css'; 
 
 
@@ -31,18 +32,27 @@ const ViewQuestionnaire = () => {
           {data.questions && data.questions.length > 0 ? (
             data.questions.map((question, i) => (
               <div key={i} className="question">
-                <p className="question-text">Question: {question.question_text}</p>
+                <p className="question-text">Question {i + 1}: {question.question_text}</p>
                 {question.options && question.options.length > 0 && (
                   <ul className="options">
                     {question.options.map((option, j) => (
                       <li key={j} className="option">
                         <label>
                           <input
-                            type="radio"
+                            type={i === data.questions.length - 1 ? "checkbox" : "radio"}
                             name={`question-${i}`}
                             value={option.option_text}
                           />
                           {option.option_text}
+                      {i === data.questions.length - 1 && option.option_text === 'Snapchat' && (
+                        <span style={{ marginLeft: '4px' }}><FaSnapchat /></span>
+                      )}
+                      {i === data.questions.length - 1 && option.option_text === 'Instagram' && (
+                        <span style={{ marginLeft: '4px' }}><FaInstagram /></span>
+                      )}
+                      {i === data.questions.length - 1 && option.option_text === 'X' && (
+                        <span style={{ marginLeft: '4px' }}><FaTwitter /></span>
+                      )}
                         </label>
                       </li>
                     ))}
