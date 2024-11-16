a6e57a139b6e045a91d4c2b2dfd16fbdecfcdff8
rmpan001 <rmpan001@odu.edu>
2024-11-16 15:00:11 -0500

    created models for tags and templatetag
    created model for tags for templates also created an associative table that represents the many to many relationship between tag and template.

--- phisecure/database/models/template.py
+++ phisecure/database/models/template.py
@@ -4,6 +4,7 @@ import enum
 from datetime import datetime, timezone
 from backend.project import db
 from sqlalchemy import Enum
+from sqlalchemy.orm import relationship
 
 
 class DifficultyLevel(enum.Enum):
@@ -39,7 +40,6 @@ class Template(db.Model):
     name = db.Column(db.String(120), nullable=False, unique=True, index=True)
     description = db.Column(db.String(400), nullable=False)
     category = db.Column(db.String(250))
-    tags = db.Column(db.String(120), nullable=False, index=True)
     difficulty_level = db.Column(Enum(DifficultyLevel), nullable=False)
     sender_template = db.Column(db.String(120), nullable=False)
     subject_template = db.Column(db.String(120), nullable=False)
@@ -48,6 +48,7 @@ class Template(db.Model):
     template_redflag=db.Column(db.Text, nullable=True)
     
     phishing_emails = db.relationship("PhishingEmail", back_populates="template", lazy=True)
+    tags = db.relationship("Tag", secondary="template_tags", back_populates="templates")
     
     def serialize(self):
         """
@@ -66,4 +67,35 @@ class Template(db.Model):
             'link': self.link, 
             'template_redflag': self.template_redflag
 
+        }
+        
+class Tag(db.Model):
+    """can represent keyword associated with a user based on questionnaire answers and phishing templates
+    Args:
+        db (_type_): _description_
+    """
+    # create id column and name
+    __tablename__ = "tags"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False, unique=True)
+    templates = relationship("Template", secondary="template_tags", back_populates="tags")
+    
+    
+
+class TemplateTag(db.Model):
+    """Association table for many-to-many relationship between Template and Tag
+    Args:
+        db (_type_): _description_
+    """
+    __tablename__ = "template_tags"
+    template_id = db.Column(db.Integer, db.ForeignKey('phishing_templates.id'), primary_key=True)
+    tag_id = db.Column(db.Integer, db.ForeignKey('tags.id'), primary_key=True)
+    
+    def serialize(self):
+        """
+        Convert model of a template tag into a serializable dictionary
+        """
+        return {
+            'template_id': self.template_id,
+            'tag_id': self.tag_id
         }
